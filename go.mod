9bdf87a3feef739c6ea7007cbce359b89ba3543f
index <leflan1@users.noreply.github.com>
2021-12-17 09:55:22 +0800

    up

---
+++ go.mod
@@ -0,0 +1,5 @@
+module example.com/m/v2
+
+go 1.16
+
+require github.com/xuri/excelize/v2 v2.4.1 // indirect
--- go/GetSheetNameToExcel.go
+++ go/GetSheetNameToExcel.go
@@ -1,5 +1,8 @@
-
-
+import {
+	"fmt"
+	"github.com/xuri/excelize/v2"
+}
 
 func main() {
+	fmt.print('123')
 }
