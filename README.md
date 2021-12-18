5feecdcd02bde0389a838024f5f24028a8a2cef1
index <leflan1@users.noreply.github.com>
2021-12-18 08:50:49 +0800

    12-18

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-获取excel中所有sheet名称
+### 1.获取excel中所有sheet名称
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "tool/golang"
+import (
+	"fmt"
+	"tool/golang"
+)
 
 func main() {
 	d := golang.GetFileList()
-	golang.Traversal(&d)
-	a := "C:\\Users\\lenovo\\OneDrive\\桌面\\宇森\\物料-2021-12-16\\精工.xlsx"
-	data := golang.GetSheetName(a)
-	golang.Traversal(&data)
+	for i := range d {
+		data := golang.GetSheetName(d[i])
+		fmt.Print(d[i], "\n")
+		golang.Traversal(&data)
+	}
 }
