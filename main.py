935ee9aedb12bf4868bc01446462da7f6b8a6ef0
index <leflan1@users.noreply.github.com>
2022-01-27 16:28:29 +0800

    01-22

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import python.GetSheetNameToExcel
+import python.OpencvTest
+
+
+def main():
+    # python.GetSheetNameToExcel.GetSheetNameToFile()
+    # opencv 学习
+    python.OpencvTest.opencvtest()
 
 
 if __name__ == '__main__':
     main()
-
