a4df6c55d696ce97e6799b5495d89dddc5b1547a
index <leflan1@users.noreply.github.com>
2022-02-10 15:20:22 +0800

    01-22

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import python.GetSheetNameToExcel
 import python.OpencvTest
+import python.PDFToIMG
 
 
 def main():
     # python.GetSheetNameToExcel.GetSheetNameToFile()
     # opencv 学习
-    python.OpencvTest.opencvtest()
+    # python.OpencvTest.opencvtest()
+    # pdf转图片
+    python.PDFToIMG.pdf2img(filename=r'D:/临时文件/gwt流程图.pdf')
+    # imgtopdf(input_paths="./", outputpath="./docimg.pdf", file_type=".png")
+    print("\n转换完成！")
 
 
 if __name__ == '__main__':
